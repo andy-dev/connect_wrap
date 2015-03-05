d34da2fc73fbda8cbdfeec75fe29993e2bbb490d
OaklandRocks & andy-dev <pair+OaklandRocks.andy-dev@devbootcamp.com>
2015-03-05 12:08:44 -0800

    working on test board

---
+++ piece.js
@@ -0,0 +1,21 @@
+/*
+pseudocode:
+we need two pieces
+we need to know whether they are red or black pieces
+the piece should know where they are
+
+var redPiece = function(){
+
+}
+var bluePiece = function(){
+
+}
+
+*/
+
+var RedPiece = function(){
+  this.color = red
+}
+var BluePiece = function(){
+  this.color = blue
+}
