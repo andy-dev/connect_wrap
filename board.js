96b51ceab85fda958b097dce9f0015925ed214e2
Walter Zimmerman <zimmermanw84@gmail.com>
2015-03-06 15:40:33 -0800

    wip

--- board.js
+++ board.js
@@ -8,10 +8,13 @@
 
   "use strict";
 
+  var connectMore = new Firebase("https://luminous-inferno-9321.firebaseio.com");
   var rows = $("section");
   var turnCounter = 0;
   var colors;
   var playerColor;
+  var boardString;
+  var updatedBoard;
 
   // Choose whos turn it is
   var decideTurn = function() {
@@ -21,7 +24,6 @@
     function moveCounter(){
       playerColor = colors[turnCounter];
       playerColor === "red" ? turnCounter++ : turnCounter--;
-      console.log(playerColor);
     };
 
     function bindCounterEvent() {
@@ -44,6 +46,8 @@
 
         if (cell.hasClass('')) {
           cell.addClass(playerColor);
+          cell.hide();
+          cell.fadeIn(2000)
           return;
         }
       }
@@ -56,31 +60,89 @@
     cellColorEventBind();
   };
 
+  var renderFireBoard = function() {
+
+    if (updatedBoard == null) return;
+
+    updatedBoard = updatedBoard.split('')
+    rows.each(function(index, row){
+        for (var i=0; i < 47; i++){
+          if (updatedBoard[i] == "r"){
+            console.log($(row.children[i]).addClass("red"));
+            // console.log(row.children)
+          } else if (updatedBoard[i] == "b") {
+            $(row.children[i]).addClass("black");
+            console.log(row.children)
+          }
+        }
+      })
+
+  };
+
+
   // Create String that will be used to pick winner
 
   var createBoardString = function() {
 
-    var boardString = "";
+    function updateString() {
 
-    rows.each(function(index, row){
-      for (var i=0; i < row.children.length; i++){
-        if (row.children[i].className == "red"){
-          boardString += "r";
-        } else if (row.children[i].className == "black") {
-          boardString += "b";
-        } else {
-          boardString += "x";
+      boardString = ""
+
+      rows.each(function(index, row){
+        for (var i=0; i < row.children.length; i++){
+          if (row.children[i].className == "red"){
+            boardString += "r";
+          } else if (row.children[i].className == "black") {
+            boardString += "b";
+          } else {
+            boardString += "x";
+          }
         }
-      }
-    });
-    // return boardString;
-    console.log(boardString); // Testing
+      })
+      return boardString
+    };
+    // boardString; // Testing
+    function updateFirebase() {
+      $("#boardstring").text(boardString);
+      // connectMore.set({ board: boardString })
+      connectMore.update({ board: boardString });
+      connectMore.on("value", function(data) {
+      updatedBoard = data.val().board;
+      renderFireBoard;
+      // console.log(boardString);// ? data.val().boardString : "";
+      // console.log("My Board String " + boardString);
+      });
+    };
+
+
+    function bindUpdateClick() {
+      $(document).on('click', updateString)
+      $(document).on('click', updateFirebase);
+      $('button').on('click', renderFireBoard);
+    };
+
+
+    bindUpdateClick()
   };
+
+  // var syncWithPlayer = function() {
+
+
+
+  //   function bindSyncEvent() {
+  //   $('button').on('click', renderFireBoard);
+
+  //   }
+
+  //   bindSyncEvent()
+  // };
 
 
   var init = function() {
     cellColorDeligation();
     decideTurn();
+    createBoardString();
+    // syncWithPlayer();
   };
 
   window.gameModule = function() {
