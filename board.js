26a7a5090208f140c9132448e262315de6e149f0
Walter Zimmerman <zimmermanw84@gmail.com>
2015-03-05 18:58:05 -0800

    module attempt

--- board.js
+++ board.js
@@ -2,15 +2,17 @@
 //1. Board object
 //  columns, rows
 
-var Board = function(){
-  this.columns = 7;
-  this.rows = 6;
-};
+// var Board = function(){
+//   this.columns = 7;
+//   this.rows = 6;
+// };
 
 var color = ["black", "red"];
 
 var turn = color[i];
 var i = 0;
+
+
 
 $(document).on("click", function(){
 
@@ -57,3 +59,88 @@ rows.on("click", function(){
 
   }
 })
+
+// Game Module
+(function() {
+
+  "use strict";
+
+  var rows = $("section");
+  var color = ["black", "red"];
+  var i = 0;
+  var turn;
+
+
+  // Choose whos turn it is
+  var decideTurn = function() {
+
+    turn = color[i];
+
+    function moveCounter(){
+      if (turn === "red") ? i++ : i--;
+    };
+
+    function bindCounterEvent() {
+      $(document).on("click", moveCounter);
+    };
+
+    bindCounterEvent();
+  };
+
+  // Create String that will be used to pick winner
+
+  var createBoardString = function() {
+
+    var boardString = "";
+
+    rows.each(function(index, row){
+      for (var i=0; i < row.children.length; i++){
+        if (row.children[i].className == "red"){
+          boardString += "r";
+        } else if (row.children[i].className == "black") {
+          boardString += "b";
+        } else {
+          boardString += "x";
+        }
+      }
+    });
+    return boardString;
+  };
+
+  // Choose What Color is placed
+
+  var cellColorDeligation = function() {
+
+    var column = $(this).children();
+
+    function decidePiecePlaced(){
+      for (var i=5; i >= 0; i--){
+        // red is going to be color var later
+        var cell = $(column[i]);
+        if (!cell.hasClass(turn)) {
+          cell.addClass(turn);
+          return;
+        }
+      }
+    };
+
+    function cellColorEventBind() {
+      rows.on("click", decidePiecePlaced)
+    };
+
+    cellColorEventBind();
+  };
+
+  var init = function() {
+    cellColorDeligation();
+    decideTurn();
+    createBoardString();
+  };
+
+  window.gameModule = function() {
+    return {
+      init: init,
+    }
+  };
+
+})()
